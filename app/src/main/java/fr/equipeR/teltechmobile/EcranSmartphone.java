4053c90bc88e8f9435f60e2759408e095bbed44a
leajiner <leajiner@gmail.com>
2023-03-31 10:10:45 +0200

    Création de l'écran du smartphone

---
+++ app/src/main/java/fr/equipeR/teltechmobile/EcranSmartphone.java
@@ -0,0 +1,14 @@
+package fr.equipeR.teltechmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EcranSmartphone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecran_smartphone);
+    }
+}
