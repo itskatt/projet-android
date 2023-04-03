0358977706201556ee69d3cac37d0bde84b7dcad
Itskatt <raph10@mail.com>
2023-04-03 22:05:06 +0200

    Relier l'écran d'informations de payement à l'écran de confirmation de payement

--- app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
+++ app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
@@ -1,5 +1,6 @@
 package fr.equipeR.teltechmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PaymentInfoActivity extends ShakeableActivity {
@@ -8,5 +9,10 @@ public class PaymentInfoActivity extends ShakeableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_info);
+
+        findViewById(R.id.payButton).setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), EcranConfirmation.class);
+            startActivity(intent);
+        });
     }
 }
