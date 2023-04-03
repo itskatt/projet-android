f1253944e33071da0d1e73e9d56b75039f0f3d9b
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-04-03 14:26:41 +0200

    Ajouter le boutton de panier dans la navbar

--- app/src/main/java/fr/equipeR/teltechmobile/MainActivity.java
+++ app/src/main/java/fr/equipeR/teltechmobile/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity implements SmartphoneParentA
 
         SmartphoneList list = SmartphoneList.getInstance();
         list.fetchSmartphones(this::onSmartphonesFetched, System.err::println);
+
+        findViewById(R.id.cartIcon).setOnClickListener(v -> {
+            Toast.makeText(this, "Panier...", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void onSmartphonesFetched(List<Smartphone> s) {
@@ -43,5 +48,4 @@ public class MainActivity extends AppCompatActivity implements SmartphoneParentA
     public Context getContext() {
         return getApplicationContext();
     }
-
 }
