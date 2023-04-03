88fc77754131e5d18862d2fc8719fb40e7601615
Itskatt <raph10@mail.com>
2023-04-03 22:15:40 +0200

    Validation des données avant de confirmer le payement

--- app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
+++ app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
@@ -2,6 +2,8 @@ package fr.equipeR.teltechmobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PaymentInfoActivity extends ShakeableActivity {
 
@@ -10,9 +12,26 @@ public class PaymentInfoActivity extends ShakeableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_info);
 
+        TextView surname = findViewById(R.id.formNom);
+        TextView firstname = findViewById(R.id.formPrenom);
+        TextView shippingAdress = findViewById(R.id.formAdresse);
+
         findViewById(R.id.payButton).setOnClickListener(view -> {
+            if (checkIfInvalid(surname, "Le nom")) return;
+            if (checkIfInvalid(firstname, "Le prénom")) return;
+            if (checkIfInvalid(shippingAdress, "L'adresse de livraison")) return;
+
             Intent intent = new Intent(getApplicationContext(), EcranConfirmation.class);
             startActivity(intent);
         });
+    }
+
+    private boolean checkIfInvalid(TextView view, String name) {
+        if (view.getText().length() == 0) {
+            Toast.makeText(this, name + " ne peut pas être vide", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return false;
     }
 }
