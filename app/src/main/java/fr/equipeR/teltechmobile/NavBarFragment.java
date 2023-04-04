2f1be543a783881a19abd705b45f89e71e74b62e
leajiner <leajiner@gmail.com>
2023-04-04 15:16:53 +0200

    Ajout de la javadoc pour la MainActivity, NavBarFragment, PaymentInfoActivity, ShakeableActivity

--- app/src/main/java/fr/equipeR/teltechmobile/NavBarFragment.java
+++ app/src/main/java/fr/equipeR/teltechmobile/NavBarFragment.java
@@ -9,17 +9,37 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+/**
 
+  * Classe représentant un fragment de barre de navigation contenant un icône de panier.
+ */
 public class NavBarFragment extends Fragment {
 
+    /**
+     *
+     * Constructeur par défaut de la classe NavBarFragment.
+     */
     public NavBarFragment() {
     }
 
+    /**
+     *
+     * Méthode appelée lors de la création de l'activité contenant le fragment.
+     *
+     * @param savedInstanceState état enregistré de l'activité
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     *
+     * Méthode appelée lorsque la vue associée au fragment a été créée.
+     *
+     * @param view vue associée au fragment
+     * @param savedInstanceState état enregistré de la vue
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -29,6 +49,16 @@ public class NavBarFragment extends Fragment {
         });
     }
 
+    /**
+     *
+     * Méthode appelée pour créer et retourner la vue associée au fragment.
+     *
+     * @param inflater objet permettant de créer la vue à partir d'un layout XML
+     * @param container conteneur parent de la vue
+     * @param savedInstanceState état enregistré de la vue
+     *
+     * @return la vue créée pour le fragment
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_nav_bar, container, false);
--- app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
+++ app/src/main/java/fr/equipeR/teltechmobile/PaymentInfoActivity.java
@@ -4,9 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
-
+/**
+ *
+ * Activité permettant de saisir les informations de paiement et de les vérifier avant de passer à l'écran de confirmation.
+ */
 public class PaymentInfoActivity extends ShakeableActivity {
 
+    /**
+     *
+     * Méthode appelée lors de la création de l'activité.
+     * Initialise les champs de saisie et ajoute un écouteur de clic sur le bouton de paiement.
+     *
+     * @param savedInstanceState état enregistré de l'activité
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +36,21 @@ public class PaymentInfoActivity extends ShakeableActivity {
         });
     }
 
+    /**
+     *
+     * Vérifie si un champ de saisie est valide.
+     * Affiche un message d'erreur si le champ est vide.
+     *
+     * @param view champ de saisie à vérifier
+     * @param name nom du champ de saisie
+     *
+     * @return true si le champ est invalide, false sinon
+     */
     private boolean checkIfInvalid(TextView view, String name) {
         if (view.getText().length() == 0) {
             Toast.makeText(this, name + " ne peut pas être vide", Toast.LENGTH_SHORT).show();
             return true;
         }
-
         return false;
     }
 }
