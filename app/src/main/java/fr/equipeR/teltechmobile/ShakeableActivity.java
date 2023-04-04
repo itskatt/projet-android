803ecbe171e7d2100e1e8b5d5b30b7e1ee6ef912
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-04-04 15:07:42 +0200

    Moin de warnings

--- app/src/main/java/fr/equipeR/teltechmobile/ShakeableActivity.java
+++ app/src/main/java/fr/equipeR/teltechmobile/ShakeableActivity.java
@@ -1,7 +1,6 @@
 package fr.equipeR.teltechmobile;
 
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
